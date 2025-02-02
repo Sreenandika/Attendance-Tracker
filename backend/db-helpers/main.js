8d93aef2b57fa7c8d81a54201a2ff7860c6a7228
Nandagopal SB <nandagopal.nsb@gmail.com>
2025-02-02 21:15:27 +0530

    add department management functionality and update UI elements

--- backend/db-helpers/main.js
+++ backend/db-helpers/main.js
@@ -1,29 +1,37 @@
-
-function getAdminUsers (client) {
-    const query = `SELECT user_id,username,password FROM user_accounts WHERE user_role = 'admin';`;
-    return client.query(query);
+function getAdminUsers(client) {
+	const query = `SELECT user_id,username,password FROM user_accounts WHERE user_role = 'admin';`;
+	return client.query(query);
 }
-function getStudentUsers (client) {
-    const query = `SELECT username,password FROM user_accounts WHERE user_role = 'student';`;
-    return client.query(query);
+function getStudentUsers(client) {
+	const query = `SELECT username,password FROM user_accounts WHERE user_role = 'student';`;
+	return client.query(query);
 }
-function addClasses(client,class_name,class_id){
-    const query = `INSERT INTO classes(class_id,class_name) VALUES(${class_id},'${class_name}');`;
-    return client.query(query);
-
+function addClasses(client, class_name, class_id) {
+	const query = `INSERT INTO classes(class_id,class_name) VALUES(${class_id},'${class_name}');`;
+	return client.query(query);
 }
-function editClass(client,class_name,new_class_name,class_id,new_class_id){
-    const query = `UPDATE classes SET class_id = ${new_class_id}, class_name = '${new_class_name}' WHERE class_id = ${class_id} AND class_name = '${class_name}';`;
-    return client.query(query);
+function editClass(client, class_name, new_class_name, class_id, new_class_id) {
+	const query = `UPDATE classes SET class_id = ${new_class_id}, class_name = '${new_class_name}' WHERE class_id = ${class_id} AND class_name = '${class_name}';`;
+	return client.query(query);
 }
-function getClass (client) {
-    const query = `SELECT class_name from classes;`;
-    return client.query(query);
+function getClass(client) {
+	const query = `SELECT class_name from classes;`;
+	return client.query(query);
+}
+function addDepartments(client, department_id, department_name) {
+	const query = `INSERT INTO departments (department_id,department_name) VALUES(${department_id},'${department_name}');`;
+	return client.query(query);
+}
+function getDepartments(client) {
+	const query = `SELECT department_name from departments;`;
+	return client.query(query);
 }
 module.exports = {
-    getClass,
-    editClass,
-    getAdminUsers,
-    getStudentUsers,
-    addClasses
-}
+	getClass,
+	editClass,
+	getAdminUsers,
+	getStudentUsers,
+	addClasses,
+	addDepartments,
+	getDepartments,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,111 +1,142 @@
-const express = require('express');
-const { Pool } = require('pg');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { Pool } = require("pg");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const funcs = require('./db-helpers/main.js')
-const db = require('./db-helpers/const.js');
+const funcs = require("./db-helpers/main.js");
+const db = require("./db-helpers/const-local.js");
 const app = express();
 const port = 3000;
 
 const pool = new Pool({
-    user: db.user,
-    host: db.host,
-    database: db.database,
-    password: db.password,
-    port: db.port,
-    ssl : db.ssl
+	user: db.user,
+	host: db.host,
+	database: db.database,
+	password: db.password,
+	port: db.port,
+	ssl: db.ssl,
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public", 'index.html'));
-}
-);
-
-app.get('/students', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/pages/students", 'students.html'));
-}
-);
-
-
-app.get('/admin/:admin_id', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/pages/admin", 'admin.html'));
-});
-app.get('/admin/:admin_id/student_manager', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/pages/admin", 'admin.html'));
-});
-app.get('/admin/:admin_id/teacher_manager', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/pages/admin", 'admin.html'));
-});
-app.get('/admin/:admin_id/class_manager', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/pages/admin", 'classHandler.html'));
-});
-app.get('/admin/:admin_id/subject_manager', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/pages/admin", 'subjectHandler.html'));
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-
-app.post('/login', async (req, res) => {
-    const client = await pool.connect();
-    if(req.body.userType === 'admin'){
-        const dataToSend = {
-            user_id: 'no_user_found',
-            loginStatus: false
-        };
-        funcs.getAdminUsers(client)
-        .then((result) => {
-            console.log(result.rows);
-            for(let i = 0; i < result.rows.length; i++){
-                if(req.body.username === result.rows[i].username && req.body.password === result.rows[i].password){
-                    dataToSend.user_id = result.rows[i].user_id;
-                    dataToSend.loginStatus = true;
-                    break;
-                }
-            }
-            res.send(dataToSend);
-        })
-        .catch((e) => {
-            console.error('Error getting admin users', e);
-            res.send('Error getting admin users');
-        });
-    }
-    client.release();
+app.get("/students", (req, res) => {
+	res.sendFile(
+		path.join(__dirname, "../public/pages/students", "students.html")
+	);
 });
 
-app.get('/getClass', async (req, res) => {
-    const client = await pool.connect();
-    funcs.getClass(client) .then((result) => {
-        res.send(result.rows);
-    });
-    client.release();
+app.get("/admin/:admin_id", (req, res) => {
+	res.sendFile(path.join(__dirname, "../public/pages/admin", "admin.html"));
+});
+app.get("/admin/:admin_id/student_manager", (req, res) => {
+	res.sendFile(path.join(__dirname, "../public/pages/admin", "admin.html"));
+});
+app.get("/admin/:admin_id/teacher_manager", (req, res) => {
+	res.sendFile(path.join(__dirname, "../public/pages/admin", "admin.html"));
+});
+app.get("/admin/:admin_id/class_manager", (req, res) => {
+	res.sendFile(
+		path.join(__dirname, "../public/pages/admin", "classHandler.html")
+	);
+});
+app.get("/admin/:admin_id/subject_manager", (req, res) => {
+	res.sendFile(
+		path.join(__dirname, "../public/pages/admin", "subjectHandler.html")
+	);
+});
+app.get("/admin/:admin_id/department_manager", (req, res) => {
+	res.sendFile(
+		path.join(__dirname, "../public/pages/admin", "departmentHandler.html")
+	);
 });
 
-
-app.post('/addClass', async (req, res) => {
-    const client = await pool.connect();
-    funcs.addClasses(client,req.body.className,req.body.classId).then((result) => {
-        console.log(result.rows);
-    });
-    client.release();
+app.post("/login", async (req, res) => {
+	const client = await pool.connect();
+	if (req.body.userType === "admin") {
+		const dataToSend = {
+			user_id: "no_user_found",
+			loginStatus: false,
+		};
+		funcs
+			.getAdminUsers(client)
+			.then((result) => {
+				console.log(result.rows);
+				for (let i = 0; i < result.rows.length; i++) {
+					if (
+						req.body.username === result.rows[i].username &&
+						req.body.password === result.rows[i].password
+					) {
+						dataToSend.user_id = result.rows[i].user_id;
+						dataToSend.loginStatus = true;
+						break;
+					}
+				}
+				res.send(dataToSend);
+			})
+			.catch((e) => {
+				console.error("Error getting admin users", e);
+				res.send("Error getting admin users");
+			});
+	}
+	client.release();
 });
 
-app.post('/editClass', async (req, res) => {
-    const client = await pool.connect();
-    funcs.editClass(client,req.body.oldClass,req.body.className,req.body.oldClassId,req.body.classId).then((result) => {    
-        console.log(result.rows);
-    }
-);
-    client.release();
+app.get("/getClass", async (req, res) => {
+	const client = await pool.connect();
+	funcs.getClass(client).then((result) => {
+		res.send(result.rows);
+	});
+	client.release();
 });
 
-app.get('/pictures/login', (req, res) => {
-res.sendFile(path.join(__dirname, "../public/pages/pictures", 'person.png'));
+app.post("/addClass", async (req, res) => {
+	const client = await pool.connect();
+	funcs
+		.addClasses(client, req.body.className, req.body.classId)
+		.then((result) => {
+			console.log(result.rows);
+		});
+	client.release();
 });
 
+app.post("/editClass", async (req, res) => {
+	const client = await pool.connect();
+	funcs
+		.editClass(
+			client,
+			req.body.oldClass,
+			req.body.className,
+			req.body.oldClassId,
+			req.body.classId
+		)
+		.then((result) => {
+			console.log(result.rows);
+		});
+	client.release();
+});
 
+app.post("/addDepartment", async (req, res) => {
+	console.log(req.body);
+	const client = await pool.connect();
+	funcs
+		.addDepartments(client, req.body.departmentId, req.body.departmentName)
+		.then((result) => {
+			res.send("OK");
+		});
+	client.release();
+});
+app.get("/getDepartments", async (req, res) => {
+	const client = await pool.connect();
+	funcs.getDepartments(client).then((result) => {
+		res.send(result.rows);
+	});
+	client.release();
+});
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
